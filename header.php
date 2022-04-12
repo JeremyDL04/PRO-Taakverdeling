17cfce951fcb970e5abc3bc9771658ced69e42b9
Jeremy de Leeuw <jeremydeleeuw@gmail.com>
2022-04-12 12:20:32 +0200

    Changed header & footer.php

--- header.php
+++ header.php
@@ -2,9 +2,10 @@
 	<div class="wrapper">
 		<a href="index.php"><img src="img/achtbaan1.png" alt="logo" class="logo"></a>
 			<nav>
+		<a href="index.php"><img src="img/curio.png" alt="logo" class="logo"></a>
+		<nav>
 			<a href="index.php">Home</a>
-			<a href="hobbies.php">Hobbies</a>
-			<a href="aboutme.php">About Me</a>
+			<a href="info.php">Info</a>
 			<a href="contact.php">Contact</a>
 		</nav>
 	</div>
