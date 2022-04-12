8208b3a5ac1ecda66edb89b3220b71a5398ac5b9
Jeremy de Leeuw <jeremydeleeuw@gmail.com>
2022-04-12 12:21:47 +0200

    Added info.php

---
+++ info.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Homepage</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com">
+	<link rel="stylesheet" href="fontawesome/css/all.min.css">
+</head>
+
+<?php require_once 'header.php'; ?>
+
+<main>
+	<div class="balk1">
+		<img src="img/balk1.png" alt="balk" class="balk2">
+	</div>
+</main>
+
+<?php require_once 'footer.php'; ?>
+	
+</body>
+</html>
